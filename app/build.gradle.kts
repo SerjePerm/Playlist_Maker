7029de2f633b8b24d9e93e5779a84499f1e39ae9
Сергей Михалёв <snt_mail@bk.ru>
2023-12-02 14:56:32 +0500

    sprint #11 commit #1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.playlistmaker"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.playlistmaker"
@@ -36,10 +36,13 @@ android {
 }
 
 dependencies {
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.google.code.gson:gson:2.10.1")
     implementation("com.github.bumptech.glide:glide:4.16.0")
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/com/example/playlistmaker/utils/Utils.kt
+++ app/src/main/java/com/example/playlistmaker/utils/Utils.kt
@@ -2,7 +2,11 @@ package com.example.playlistmaker.utils
 
 import android.content.Context
 import android.util.TypedValue
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 fun dpToPx(dp: Int, context: Context): Int {
     return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp.toFloat(), context.resources.displayMetrics).toInt()
 }
+
+fun msToTime(ms: Int): String = SimpleDateFormat("mm:ss", Locale.getDefault()).format(ms)
