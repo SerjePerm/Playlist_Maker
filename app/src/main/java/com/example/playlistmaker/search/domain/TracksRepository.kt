056c8c2ef1c353d2d5528b9da205e8fd8821ee23
Сергей Михалёв <snt_mail@bk.ru>
2024-05-01 13:38:56 +0500

    sprint #20 commit #1

--- app/src/main/java/com/example/playlistmaker/search/domain/TracksRepository.kt
+++ app/src/main/java/com/example/playlistmaker/search/domain/TracksRepository.kt
@@ -1,7 +1,8 @@
 package com.example.playlistmaker.search.domain
 
 import com.example.playlistmaker.search.domain.models.TracksResponse
+import kotlinx.coroutines.flow.Flow
 
 interface TracksRepository {
-    fun searchTracks(expression: String): TracksResponse
+    fun searchTracks(expression: String): Flow<TracksResponse>
 }
